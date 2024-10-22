d65267f3427737b60d591a1e1eeeea4bdd7fdac5
Vlad <dr.melix@gmail.com>
2024-10-23 00:15:19 +0300

    Починил фронт

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    host: true, // чтобы Vite слушал на 0.0.0.0
+  },
   plugins: [react()],
-})
+});
